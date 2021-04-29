6e8d6cbf0c37f45cd177cd6da3ec02b9ff0d78fe
Enrico <enrico.mazzucchelli@vipera.com>
2021-04-29 12:37:35 +0200

    create project structure

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# cc-csv
+Si implementi la funzione customPrint della classe Solutions
+
+~~~~
+    /**
+     * Allows the columns projection of the input data
+     * Formally like: "select colums from data"
+     * @param data well-formed csv with comma as separator and \n as end of line (also  the last row ends with \n)
+     * @param columns the colums to project
+     * @return a new csv (with the same format) but that contains only the colums specified (with the specified order)
+     */
+    public String customPrint(String data, String[] columns) {}
+~~~~
+
+Dove data è una stringa contenente un csv ben formato (non ci sono colonne mancanti). La prima riga del csv contiene gli header (titoli) delle colonne
+L'array columns in input rappresenta invece l'array delle colonne di cui si vuole fare la proiezione.
+In output si vuole ottenere un csv (come stringa).
+
+
+Esempio
+~~~~
+LastName,FirstName,Age,Job
+Mazzucchelli,Enrico,30,Developer
+Devil,Andrea,52,Bella domanda
+
+~~~~
+
+Facendo la seguente richiesta
+```java
+Solution solution = new Solution();
+String output = solution.customPrint(test, new String[]{"FirstName","Age"});
+```
+
+Si ottiene come output:
+
+~~~~
+FirstName,Age
+Enrico,30
+Andrea,52
+
+~~~~
+
+
+Eccezioni:
+Il metodo generera una IllegalArgumentException se l'array di columns è nullo o vuoto
+Il metodo generera una IllegalArgumentException se l'array di columns contiene stringhe di colonne non presenti nel csv
+Il testo di quest'ultima eccezione deve essere: 
+"Missing columns: " + invalidColumnsCount
+
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SolutionTest {
+
+    @Test
+    public void noChangeTest(){
+        String csv =
+                "LastName,FirstName,Age,Job\n" +
+                        "Mazzucchelli,Enrico,30,Developer\n" +
+                        "Devil,Andrea,52,Bella domanda\n" ;
+        Solution solution = new Solution();
+        String res = solution.customPrint(csv, new String[]{"LastName","FirstName","Age","Job"});
+        assertNotNull(res, "Result is null");
+        assertEquals(csv, res );
+    }
+
+    @Test
+    public void firstTest(){
+        String test =
+                "LastName,FirstName,Age,Job\n" +
+                        "Mazzucchelli,Enrico,30,Developer\n" +
+                        "Devil,Andrea,52,Bella domanda\n" ;
+        Solution solution = new Solution();
+        String res = solution.customPrint(test, new String[]{"FirstName","Age"});
+        assertNotNull(res, "Result is null");
+        assertEquals("FirstName,Age\n" +
+                "Enrico,30\n" +
+                "Andrea,52\n", res );
+    }
+
+    @Test
+    public void singleColumnTest(){
+        String test =
+                "LastName,FirstName,Age,Job\n" +
+                        "Mazzucchelli,Enrico,30,Developer\n" +
+                        "Devil,Andrea,52,Bella domanda\n" ;
+        Solution solution = new Solution();
+        String res = solution.customPrint(test, new String[]{"FirstName"});
+        assertNotNull(res, "Result is null");
+        assertEquals("FirstName\n" +
+                "Enrico\n" +
+                "Andrea\n", res);
+    }
+
+    @Test
+    public void illegalArguments(){
+        String test =
+                "LastName,FirstName,Age,Job\n" +
+                        "Mazzucchelli,Enrico,30,Developer\n" +
+                        "Devil,Andrea,52,Bella domanda\n" ;
+        Solution solution = new Solution();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            solution.customPrint(test, new String[]{});
+        });
+    }
+
+}
