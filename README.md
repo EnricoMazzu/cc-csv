3fe5f7d89310aa2c942fc0c28b6f87ab0bc5fb98
Enrico <enrico.mazzucchelli@vipera.com>
2021-04-29 12:54:52 +0200

    Add skeleton

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Si implementi la funzione customPrint della classe Solutions
      * Allows the columns projection of the input data
      * Formally like: "select colums from data"
      * @param data well-formed csv with comma as separator and \n as end of line (also  the last row ends with \n)
-     * @param columns the colums to project
+     * @param columns the columns to extract
      * @return a new csv (with the same format) but that contains only the colums specified (with the specified order)
      */
     public String customPrint(String data, String[] columns) {}
---
+++ src/main/java/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public static final String COMMA_SEPARATOR = ",";
+    public static final String END_LINE_DELIMITER = "\n";
+
+    /**
+     * Allows the columns projection of the input data
+     * Formally like: "select colums from data"
+     * @param data well-formed csv with comma as separator and \n as end of line (also  the last row ends with \n)
+     * @param columns the columns to extract
+     * @return a new csv (with the same format) but that contains only the colums specified (with the specified order)
+     */
+    public String customPrint(String data, String[] columns) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+
+}
